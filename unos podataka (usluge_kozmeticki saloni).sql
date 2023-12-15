595231c2806faf51cc04c43935d3f7ee5341aae9
Mem28 <jisse.merlin@gmail.com>
2023-12-15 15:20:27 +0100

    unos podataka(usluge_kozmeticki saloni)

---
+++ unos podataka (usluge_kozmeticki saloni).sql
@@ -0,0 +1,20 @@
+select * from usluge;
+
+
+insert into usluge (opis,cijena)
+values ('pranje/sušenje', null);
+
+insert into usluge (opis,cijena)
+values ('šišanje/feniranje', 45);
+
+insert into usluge (opis,cijena)
+values ('pramenovi/paket', 85);
+
+insert into usluge (opis,cijena)
+values ('bojanje/duga_kosa', 60);
+
+insert into usluge (opis,cijena)
+values ('pranje/sušenje', 25);
+
+insert into usluge (opis,cijena)
+values ('balayage/paket', 95);
