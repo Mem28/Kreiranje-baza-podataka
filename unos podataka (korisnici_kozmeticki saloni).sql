7bb7f3ba32a4e515d4b64059c7a9fd12a980aeae
Mem28 <jisse.merlin@gmail.com>
2023-12-15 15:09:56 +0100

    unos podataka (korisnici_kozmeticki saloni)

---
+++ unos podataka (korisnici_kozmeticki saloni).sql
@@ -0,0 +1,20 @@
+select* from korisnici;
+
+
+insert into korisnici(ime,prezime,br_tel)
+values('Dorotea', 'Dobić', '+385923346789');
+
+insert into korisnici(ime,prezime,br_tel)
+values('Lea','Topić', '+385917839992');
+
+insert into korisnici(ime,prezime,br_tel)
+values('Karmela', 'Savić', '+385951113765');
+
+insert into korisnici(ime,prezime,br_tel)
+values('Mia', 'Lukuć', '+38599330089');
+
+insert into korisnici(ime,prezime,br_tel)
+values('Sara', 'Petrov', '+385990098543');
+
+insert into korisnici(ime,prezime,br_tel)
+values('Ana Marija', 'Žarkov', '+385912218900');
