83958b68477672f4dc5732fa3735074b9f86afb8
Mem28 <jisse.merlin@gmail.com>
2023-12-15 14:54:41 +0100

    unos podataka u tablicu djelatnice(baza kozmetički saloni)

---
+++ unos podataka (djelatnice_kozmeticki saloni).sql
@@ -0,0 +1,22 @@
+select* from djelatnice;
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Matilda', 'Kraš', null,null);
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Petra', 'Milić', null,'HR12 1001 0051 8630 0016 0');
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Sonja', 'Tabrović', null,'HR1723600001101234565');
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Kristina', 'Marić', null,null);
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Anja', 'Petrović', null,null);
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Željka', 'Lukić', null,null);
+
+insert into djelatnice(ime,prezime,oib,iban)
+values('Matilda', 'Kraš', null,null);
