5008dfcb326dd5d81f48846e150225d3783626de
Mem28 <jisse.merlin@gmail.com>
2023-12-16 19:32:17 +0100

    unos podataka u BP KUD

---
+++ unos podataka u bazu podataka KUD.sql
@@ -0,0 +1,51 @@
+use kud;
+
+select * from clanovi;
+
+insert into clanovi(ime, prezime,oib)
+values ('Lara','Lukas',null);
+
+insert into clanovi(ime, prezime,oib)
+values ('Monika','Dokić',null);
+
+insert into clanovi(ime, prezime,oib)
+values ('Anđela', 'Perković',null);
+
+insert into clanovi(ime, prezime,oib)
+values ('Mirjana','Pešić',null);
+
+insert into clanovi(ime, prezime,oib)
+values ('Ana','Benčić',null);
+
+
+select *from mjesta;
+
+insert into mjesta(zemlja,inozemstvo)
+values ('DA','NE');
+
+insert into mjesta(zemlja,inozemstvo)
+values('NE','DA');
+
+
+select *from nastupi;
+
+insert into nastupi(naziv,datum)
+values ('KUD Uljanik i gosti', null);
+
+insert into nastupi(naziv,datum)
+values ('Vetarani', null);
+
+insert into nastupi(naziv,datum)
+values ('Večer s tomislavcima', null);
+
+insert into nastupi(naziv,datum)
+values ('Mojih prvih 30 godina', null);
+
+
+
+
+
+
+
+
+
