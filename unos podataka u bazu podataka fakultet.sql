3e88fa1c7041ecea90e63a98430e94fe88e828f1
Mem28 <jisse.merlin@gmail.com>
2023-12-16 10:58:21 +0100

    unos podataka u BP fakultet

---
+++ unos podataka u bazu podataka fakultet.sql
@@ -0,0 +1,83 @@
+use fakulteti;
+
+
+select * from kolegiji;
+
+
+insert into kolegiji(naziv)
+values ('gospodarska diplomacija');
+
+insert into kolegiji(naziv)
+values ('indstrijska organizacija');
+
+insert into kolegiji(naziv)
+values ('mikroekonomija');
+
+insert into kolegiji(naziv)
+values ('mikroekonomska teorija');
+
+insert into kolegiji(naziv)
+values ('napredna mikroekonomija');
+
+insert into kolegiji(naziv)
+values ('osnove ekonomije');
+
+insert into kolegiji(naziv)
+values ('politička ekonomija');
+
+insert into kolegiji(naziv)
+values ('politička ekonomija globalizacije');
+
+insert into kolegiji(naziv)
+values ('politička ekonomija rasta');
+
+insert into kolegiji(naziv)
+values ('politička ekonomija misli');
+
+
+select * from rokovi;
+
+insert into rokovi(datum)
+values (null);
+
+insert into rokovi(datum)
+values (null);
+
+insert into rokovi(datum)
+values (null);
+
+insert into rokovi(datum)
+values (null);
+
+insert into rokovi(datum)
+values (null);
+
+
+
+select * from studenti;
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Toni', 'Đurić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Pero', 'Marjanić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Siniša', 'Barić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Krešo', 'Penava', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Martin', 'Ančić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Karolina', 'Stanić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Helena', 'Despić', null,null);
+
+insert into studenti (ime,prezime,oib,br_tel)
+values ('Tina', 'Marković', null,null);
+
+
