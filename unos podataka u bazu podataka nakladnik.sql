6258d63595f8aa8e6f00df13f8591f1d8cbfe305
Mem28 <jisse.merlin@gmail.com>
2023-12-17 15:36:37 +0100

    Unos podataka u BP nakladnik

---
+++ unos podataka u bazu podataka nakladnik.sql
@@ -0,0 +1,84 @@
+use nakladnik;
+
+select *from djela;
+
+insert into djela(naziv,sifra)
+values ('Sedamnaest',null);
+
+insert into djela(naziv,sifra)
+values ('Pod paljbom',null);
+
+
+insert into djela(naziv,sifra)
+values ('Snježna mećava',null);
+
+
+insert into djela(naziv,sifra)
+values ('Smrt u oluji',null);
+
+insert into djela(naziv,sifra)
+values ('Gost iznenađenja',null);
+
+
+insert into djela(naziv,sifra)
+values ('Ušutkane',null);
+
+
+insert into djela(naziv,sifra)
+values ('Visoke stijene',null);
+
+
+select * from mjesta;
+
+insert into mjesta(adresa,sifra)
+values ('Mihanovićeva 28, Zagreb', 10000);
+
+insert into mjesta(adresa,sifra)
+values ('Nova ves 23a, Zagreb', 10001);
+
+insert into mjesta(adresa,sifra)
+values ('Radnički dol 8, Zagreb', 10002);
+
+insert into mjesta(adresa,sifra)
+values ('Trnjanska cesta 35, Zagreb', 10003);
+
+insert into mjesta(adresa,sifra)
+values ('Dubrava 37, Zagreb', 10004);
+
+insert into mjesta(adresa,sifra)
+values ('Andrije Hebranga 22, Zagreb', 10005);
+
+insert into mjesta(adresa,sifra)
+values ('Hrvatske bratske zajednice 4, Zagreb', 10006);
+
+insert into mjesta(adresa,sifra)
+values ('Šulekova 23, Zagreb', 10007);
+
+
+select * from nakladnici;
+
+insert into nakladnici(naziv,telefon,email)
+values ('A.G.M. doo','01/4856 309','agm@agm.hr');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Alfa doo','01/4698 502','info@alfa.hr');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Bulaja naklada d.o.o.','01/482 2154','bulaja@bulaja.com');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Croatica','01/5570 940','info@croatica.hr');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Disput d.o.o.','01/2949 921','disput@disput.hr');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Dječja knjiga d.o.o.','01/4553 806','stela@kasmir-promet.hr');
+
+insert into nakladnici(naziv,telefon,email)
+values ('Dominović d.o.o.','01/6150 315','knjizara@dominovic.hr');
+
+
+
+
+
