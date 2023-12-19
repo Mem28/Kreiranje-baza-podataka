44737bb97ae4fbc33b56113d8b6297738c02d35f
Mem28 <jisse.merlin@gmail.com>
2023-12-19 16:43:07 +0100

    Unos podataka u BP restoran

---
+++ Unos podataka u bazu podataka restorani.sql
@@ -0,0 +1,150 @@
+use restorani;
+
+select * from jela;
+
+insert into jela(naziv,cijena)
+values('Sarma',7.20);
+
+insert into jela(naziv,cijena)
+values('Gulaš na dikveški',11.50);
+
+insert into jela(naziv,cijena)
+values('Ćevapi',8.00);
+
+insert into jela(naziv,cijena)
+values('Pljeskavica',9.00);
+
+insert into jela(naziv,cijena)
+values('Đevrek s kajmakom',10.00);
+
+insert into jela(naziv,cijena)
+values('Gurmanski uštipci',10.00);
+
+insert into jela(naziv,cijena)
+values('Gurmanska pljeskavica',15.00);
+
+
+
+select * from jelovnici;
+
+insert into jelovnici(sifra)
+values (153454);
+
+insert into jelovnici(sifra)
+values (145785);
+
+insert into jelovnici(sifra)
+values (123456);
+
+insert into jelovnici(sifra)
+values (198745);
+
+insert into jelovnici(sifra)
+values (254785);
+
+insert into jelovnici(sifra)
+values (766545);
+
+
+
+select * from kategorije;
+
+insert into kategorije(naziv)
+values ('Toplo predjelo');
+
+insert into kategorije(naziv)
+values ('Gotova jela');
+
+insert into kategorije(naziv)
+values ('Jela od mljevenog mesa');
+
+insert into kategorije(naziv)
+values ('Piletina');
+
+insert into kategorije(naziv)
+values ('Svinjetina');
+
+insert into kategorije(naziv)
+values ('Steak');
+
+insert into kategorije(naziv)
+values ('Specijaliteti od ribe i lignje');
+
+insert into kategorije(naziv)
+values ('Salate');
+
+insert into kategorije(naziv)
+values ('Deserti');
+
+insert into kategorije(naziv)
+values ('Prilozi i umaci');
+
+
+select * from pica;
+
+
+insert into pica (naziv,cijena)
+values ('Jana 0,33',2);
+
+insert into pica (naziv,cijena)
+values ('Jana 0,75',4);
+
+insert into pica (naziv,cijena)
+values ('Jamnica 0,25',2);
+
+insert into pica (naziv,cijena)
+values ('Jamnica 0,75',3.5);
+
+insert into pica (naziv,cijena)
+values ('Coca-cola 0,25 ',3);
+
+insert into pica (naziv,cijena)
+values ('Fanta 0,25',3);
+
+insert into pica (naziv,cijena)
+values ('Sprite 0,25',3);
+
+insert into pica (naziv,cijena)
+values ('Ledeni čaj 0,33',3);
+
+insert into pica (naziv,cijena)
+values ('Cedevita naranča/limun',3);
+
+insert into pica (naziv,cijena)
+values ('Ožujsko točeno 0,33',3);
+
+insert into pica (naziv,cijena)
+values ('Ožujsko točeno 0,5',4);
+
+insert into pica (naziv,cijena)
+values ('Ožujsko 0,33',4);
+
+insert into pica (naziv,cijena)
+values ('Ožujsko 0,5',5);
+
+insert into pica (naziv,cijena)
+values ('Heineken 0,33',5);
+
+insert into pica (naziv,cijena)
+values ('Cabernet sauvignon 0,1',3);
+
+insert into pica (naziv,cijena)
+values ('Merlot 0,1',3);
+
+insert into pica (naziv,cijena)
+values ('Teran 0,1',5);
+
+insert into pica (naziv,cijena)
+values ('Pošip 0,1',3);
+
+insert into pica (naziv,cijena)
+values ('Malvazija 0,1',3);
+
+insert into pica (naziv,cijena)
+values ('Rose 0,1',4);
+
+insert into pica (naziv,cijena)
+values ('Mužkat žuti 0,1',4);
+
+insert into pica (naziv,cijena)
+values ('Prossecco 0,1',4);
